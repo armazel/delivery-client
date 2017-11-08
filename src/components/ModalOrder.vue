0f3adaeb7ae1813971f3b7c8b163d1ec7d90ffb7
armazel <armazel@localhost>
2017-11-08 13:52:30 +0300

    ????? EsLint pre-commit

--- src/components/ModalOrder.vue
+++ src/components/ModalOrder.vue
@@ -536,13 +536,11 @@
 
     watch: {
       saving(value) {
-          debugger;
         this.$refs['modal'].dismissable = !value;
       },
 
       phoneControllVisible(val){
           if(!val) {
-            debugger;
             this.$store.dispatch(actions.scanClear);
           }
       },
